277e41aacd1a68c52ee61d825afaae9ab32fcc9d
Onat Ribar <onatribar@proton.me>
2024-08-22 15:39:39 +0300

    chore: set up springboot project

---
+++ alimama-springboot/src/main/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.alimama.alimamaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimamaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlimamaSpringbootApplication.class, args);
+    }
+
+}
---
+++ alimama-springboot/src/test/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimama.alimamaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimamaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
