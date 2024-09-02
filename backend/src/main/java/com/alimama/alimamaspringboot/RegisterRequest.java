7e335e01ada517c3a9927274361f8a172f142746
Onat Ribar <onatribar@proton.me>
2024-09-02 22:22:43 +0300

    register DTO

---
+++ backend/src/main/java/com/alimama/alimamaspringboot/RegisterRequest.java
@@ -0,0 +1,34 @@
+package com.alimama.alimamaspringboot;
+
+public class RegisterRequest {
+    private String fullName;
+    private String companyName;
+    private String email;
+    private String tckn;
+    private String password;
+    private String role;
+
+    public String getFullName() {return fullName;}
+
+    public void setFullName(String fullName) {this.fullName = fullName;}
+
+    public String getCompanyName() {return companyName;}
+
+    public void setCompanyName(String companyName) {this.companyName = companyName;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public String getTckn() {return tckn;}
+
+    public void setTckn(String tckn) {this.tckn = tckn;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getRole() {return role;}
+
+    public void setRole(String role) {this.role = role;}
+}
