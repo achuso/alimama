0088abae7c60fbe80af9986cd76497db6e851f15
Onat Ribar <onatribar@proton.me>
2024-09-02 23:11:36 +0300

    implement registration (and switch to localhost for now)

--- backend/src/main/java/com/alimama/alimamaspringboot/AuthController.java
+++ backend/src/main/java/com/alimama/alimamaspringboot/AuthController.java
@@ -2,32 +2,40 @@ package com.alimama.alimamaspringboot;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.AuthenticationException;
 
 @RestController
 @RequestMapping("/api/auth")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     private final AuthService authenticationService;
 
-    // Constructor injection
     public AuthController(AuthService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws AuthenticationException {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
             String token = authenticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
             return ResponseEntity.ok(new JwtResponse(token));
         }
         catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        try {
+            authenticationService.registerUser(registerRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Registration failed: " + e.getMessage());
         }
     }
 }
