dec1cccc3165c9cd5d827c99f5d9a42940df6182
Onat Ribar <onatribar@proton.me>
2024-09-03 00:21:24 +0300

    better exception handling

--- backend/src/main/java/com/alimama/alimamaspringboot/AuthController.java
+++ backend/src/main/java/com/alimama/alimamaspringboot/AuthController.java
@@ -1,5 +1,6 @@
 package com.alimama.alimamaspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +12,29 @@ import javax.naming.AuthenticationException;
 //@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
-    private final AuthService authenticationService;
+    private final AuthService authService;
 
+    @Autowired
     public AuthController(AuthService authenticationService) {
-        this.authenticationService = authenticationService;
+        this.authService = authenticationService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
-            String token = authenticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+            String token = authService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
             return ResponseEntity.ok(new JwtResponse(token));
-        }
-        catch (AuthenticationException e) {
+        } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
         }
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
         try {
-            authenticationService.registerUser(registerRequest);
+            authService.registerUser(registerRequest);
             return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
         }
         catch (Exception e) {
