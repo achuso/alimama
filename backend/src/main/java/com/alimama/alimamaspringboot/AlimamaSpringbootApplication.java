95e75e8d79644a7f9f9a04e7022f5262ca53fd9a
Onat Ribar <onatribar@proton.me>
2024-09-02 22:24:00 +0300

    rm print stmt

--- backend/src/main/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplication.java
+++ backend/src/main/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlimamaSpringbootApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getenv("POSTGRES_PORT"));
         SpringApplication.run(AlimamaSpringbootApplication.class, args);
     }
 
