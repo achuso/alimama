ec3738b8b1172886dcce89b4f346c10366804c66
Onat Ribar <onatribar@proton.me>
2024-09-03 19:03:20 +0300

    introduce token expiration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import AuthContainer from './components/Welcome-Page/AuthContainer.tsx';
 import NotFound from './components/NotFound.tsx';
--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
-// for pages that require authentication (e.g. dashboard)
-const PrivateRoute = ({ children }) => {
+interface JwtPayload {
+  exp: number;
+}
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+const isTokenExpired = (token: string): boolean => {
+  const decoded: JwtPayload = jwtDecode(token);
+  const currentTime = Math.floor(Date.now() / 1000);
+  return decoded.exp < currentTime;
+};
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const authToken = localStorage.getItem('authToken');
-  return authToken ? children : <Navigate to="/login" />;
+
+  if (!authToken || isTokenExpired(authToken)) {
+    // remove tokens if expired
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userRole');
+    localStorage.removeItem('userFullName');
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
 };
 
 export default PrivateRoute;
