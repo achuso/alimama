6e9a88ad0d24ad60e92ef5407e019375550e516d
Onat Ribar <onatribar@proton.me>
2024-09-03 17:07:34 +0300

    routing for new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-//import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import AuthContainer from './components/Welcome-Page/AuthContainer.tsx';
+import NotFound from './components/NotFound.tsx';
+import PrivateRoute from './components/PrivateRoute.tsx'; 
+import VendorDashboard from './components/VendorPage/VendorDashboard.tsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthContainer/>} />
+        <Route path="/" element={<AuthContainer />} />
+        <Route path="/login" element={<AuthContainer />} />
+        <Route path="/vendor-dashboard" element={<PrivateRoute><VendorDashboard /></PrivateRoute>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
