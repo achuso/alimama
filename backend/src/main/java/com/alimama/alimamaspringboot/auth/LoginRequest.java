4aad10618f14f486fa5e7445063f4c2dba1816db
Onat Ribar <onatribar@proton.me>
2024-09-04 10:43:47 +0300

    fix unused setter

--- backend/src/main/java/com/alimama/alimamaspringboot/auth/LoginRequest.java
+++ backend/src/main/java/com/alimama/alimamaspringboot/auth/LoginRequest.java
@@ -15,5 +15,5 @@ public class LoginRequest {
     public void setEmail(String email) {this.email = email;}
 
     public String getPassword() {return pwd_hash;}
-    public void setPassword(String password) {this.pwd_hash = pwd_hash;}
+    public void setPassword(String password) {this.pwd_hash = password;}
 }
