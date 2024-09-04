aeec436e9a9aca1a7730473756a7d11a343a538e
Onat Ribar <onatribar@proton.me>
2024-09-04 10:32:49 +0300

    cleanup

--- backend/src/main/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplication.java
+++ backend/src/main/java/com/alimama/alimamaspringboot/AlimamaSpringbootApplication.java
@@ -3,11 +3,12 @@ package com.alimama.alimamaspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.crypto.Data;
+import java.awt.image.DataBuffer;
+
 @SpringBootApplication
 public class AlimamaSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AlimamaSpringbootApplication.class, args);
     }
-
 }
