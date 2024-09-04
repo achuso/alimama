4aecfe15cfdbd0f493ff7559b9ad331ef9f56c98
Onat Ribar <onatribar@proton.me>
2024-09-04 15:15:23 +0300

    why was legalname unique???

--- database/postgresql/init.sql
+++ database/postgresql/init.sql
@@ -1,38 +1,38 @@
--- Because 'IF NOT EXISTS' for types were too hard
-DO $$ 
-BEGIN
-    CREATE TYPE user_role AS ENUM('Customer', 'Vendor', 'Admin');
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
+					-- Because 'IF NOT EXISTS' for types were too hard
+					DO $$ 
+					BEGIN
+						CREATE TYPE user_role AS ENUM('Customer', 'Vendor', 'Admin');
+					EXCEPTION
+						WHEN duplicate_object THEN null;
+					END $$;
 
-ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Customer';
-ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Vendor';
-ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Admin';
---
+					ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Customer';
+					ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Vendor';
+					ALTER TYPE user_role ADD VALUE IF NOT EXISTS 'Admin';
+					--
 
-CREATE TABLE IF NOT EXISTS users (
-	user_id SERIAL PRIMARY KEY,
-	email VARCHAR(50) UNIQUE NOT NULL,
-	tckn CHAR(11) NOT NULL, -- opt FOR int DATATYPE?
-	legal_name VARCHAR(50) UNIQUE NOT NULL,
-	user_role user_role NOT NULL,
-	created_at DATE DEFAULT CURRENT_DATE
-);
+					CREATE TABLE IF NOT EXISTS users (
+						user_id SERIAL PRIMARY KEY,
+						email VARCHAR(50) UNIQUE NOT NULL,
+						tckn CHAR(11) NOT NULL, -- opt FOR int DATATYPE?
+						legal_name VARCHAR(50) NOT NULL,
+						user_role user_role NOT NULL,
+						created_at DATE DEFAULT CURRENT_DATE
+					);
 
-CREATE TABLE IF NOT EXISTS login_info (
-	user_id SERIAL REFERENCES users,
-	pwd_hash VARCHAR(60) NOT NULL -- CONVERT USING bcrypt
-);
+					CREATE TABLE IF NOT EXISTS login_info (
+						user_id SERIAL REFERENCES users,
+						pwd_hash VARCHAR(60) NOT NULL -- CONVERT USING bcrypt
+					);
 
-CREATE TABLE IF NOT EXISTS addresses (
-	user_id SERIAL REFERENCES users,
-	address_id SERIAL PRIMARY KEY,
-	
-	label_name VARCHAR(16) NOT NULL,
-	receiver_fullname VARCHAR(64) NOT NULL,
-	address TEXT NOT NULL,
-	city CHAR(16) NOT NULL,
-	country CHAR(16) NOT NULL,
-	phone_nr CHAR(10) NOT NULL -- 0(XXX) XXX XX XX
-);
+					CREATE TABLE IF NOT EXISTS addresses (
+						user_id SERIAL REFERENCES users,
+						address_id SERIAL PRIMARY KEY,
+						
+						label_name VARCHAR(16) NOT NULL,
+						receiver_fullname VARCHAR(64) NOT NULL,
+						address TEXT NOT NULL,
+						city CHAR(16) NOT NULL,
+						country CHAR(16) NOT NULL,
+						phone_nr CHAR(10) NOT NULL -- 0(XXX) XXX XX XX
+					);
